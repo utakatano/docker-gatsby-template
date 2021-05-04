0bce760354f7c32fade0ff625c70a6ae6c66ad3e
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-05-04 11:31:17 +0900

    add-tutorial-four

---
+++ app/tutorial-part-four/src/components/layout.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { css } from "@emotion/react"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+      `}
+    >
+      <Link to={'/'}>
+        <h3
+          css={css`
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+
+      <Link
+        to={'/about/'}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+
+      { children }
+    </div>
+  )
+}
